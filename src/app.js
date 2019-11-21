4a0e210f49b5cbbdc0e2ed38bdb0c90bb830acf3
Влад <grishin.vlad23@gmail.com>
2019-11-21 16:01:39 +0200

    all in

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import 'babel-polyfill';
+import 'jquery/src/jquery';
+import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/dist/css/bootstrap.css';
+import './js/main.js';
+import './scss/base.scss';
+import './scss/header.scss';
+import './scss/footer.scss';
+import './scss/gutters.scss';
+import './scss/buttons.scss';
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader'
+                })
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
+            {
+                test: /\.(png|jpg|woff|woff2|eot|ttf|svg)$/,
+                loader: 'url-loader?limit=100000'
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('style.css')
+    ]
+};
